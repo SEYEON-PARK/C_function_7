b250a92e447d2bcda4c0164d2e6149ddbd9d05d7
SEYEON_PARK <fourleaf123@naver.com>
2021-02-11 01:12:31 +0900

    C언어 function(함수) 연습!(7)

---
+++ C_function_7.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int sum(int A)
+{
+	int S = 0;
+
+	while (A >= 1)
+	{
+		S += A;
+		A--;
+	}
+
+	return S;
+}
+
+int main(void)
+{
+	int a, result;
+
+	printf("양의 정수 한 개를 입력하시오. : ");
+	scanf_s("%d", &a);
+
+	result = sum(a);
+
+	printf("1부터 입력하신 정수까지의 정수를 모두 더하면 %d입니다.\n", result);
+
+	return 0;
+}
