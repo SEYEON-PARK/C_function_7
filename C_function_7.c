99c0820939a51185576a222efc43a68bd82acac0
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-02-12 00:29:51 +0900

    C언어 function(함수) 연습!(7)

--- C_function_7.c
+++ C_function_7.c
@@ -18,11 +18,11 @@ int main(void)
 	int a, result;
 
 	printf("양의 정수 한 개를 입력하시오. : ");
-	scanf_s("%d", &a); // 정수 
+	scanf_s("%d", &a); // 정수 입력받기
 
 	result = sum(a);
 
-	printf("1부터 입력하신 정수까지의 정수를 모두 더하면 %d입니다.\n", result);
+	printf("1부터 입력하신 정수까지의 정수를 모두 더하면 %d입니다.\n", result); // 결과 출력하기
 
 	return 0;
 }
